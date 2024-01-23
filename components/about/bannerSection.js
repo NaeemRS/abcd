45c42899cffcf117ee4341db5ae5a8547fba9ba7
NaeemRS <naeem05041996@gmail.com>
2024-01-23 18:15:23 +0500

    images

--- components/about/bannerSection.js
+++ components/about/bannerSection.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const BannerSection = ({ lable, title, text, imageUrl, reverse }) => {
     return (
-        <section className='relative h-[450px] overflow-hidden'>
-            <img src={imageUrl} alt={title} className="w-full  absolute object-cover" />
-            <div class={` ${reverse ? 'right-0 Client-ReviewsC1' : 'left-0 Client-ReviewsC'} md:w-2/3 w-full h-full Client-ReviewsC absolute`}></div>
-            <div className="2xl:max-w-[90rem] max-w-[85rem] mx-auto px-4 2xl:px-10 relative w-full lg:px-6 lg:py-20 md:py-10 py-7">
-                <div className={`flex ${reverse ? 'flex-row-reverse' : 'flex-row'} flex-wrap items-center lg:py-12 md:py-6 py-3`}>
+        <section className='relative h-[450px] overflow-hidden flex flex-col items-center justify-center'>
+            <img src={imageUrl} alt={title} className="w-full h-full absolute object-cover" />
+            <div class={` ${reverse ? 'right-0 ' : 'left-0 '} bg-[#00000071] h-full w-full  absolute`}></div>
+            <div className="2xl:max-w-[90rem] max-w-[85rem] mx-auto px-4 2xl:px-10 relative w-full lg:px-6  ">
+                <div className={`flex ${reverse ? 'flex-row-reverse' : 'flex-row'} flex-wrap items-center`}>
                     <div className="md:w-1/2 w-full text-white px-4">
                         <h2 className="text-lg font-bold redClr mb-2">{lable}</h2>
                         <h2 className="md:text-3xl text-2xl mb-3">{title}</h2>
